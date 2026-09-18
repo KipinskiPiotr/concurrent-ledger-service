e6deeeb2fb6dc8829551ddea2aa75607772a1452
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2026-09-18 10:59:49 +0200

    Init Spring Boot app

---
+++ src/main/java/com/concurrent_ledger_service/ConcurrentLedgerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.concurrent_ledger_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentLedgerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentLedgerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurrent_ledger_service/ConcurrentLedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrent_ledger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentLedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
